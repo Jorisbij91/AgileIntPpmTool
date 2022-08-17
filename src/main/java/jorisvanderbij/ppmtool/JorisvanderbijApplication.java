4dcfddb2ae4f4210d589952918eeec7bff98fdfb
Jorisbij91 <joris_bij@hotmail.com>
2022-08-17 16:02:51 +0200

    Initialized project

---
+++ src/main/java/jorisvanderbij/ppmtool/JorisvanderbijApplication.java
@@ -0,0 +1,13 @@
+package jorisvanderbij.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorisvanderbijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JorisvanderbijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jorisvanderbij/ppmtool/JorisvanderbijApplicationTests.java
@@ -0,0 +1,13 @@
+package jorisvanderbij.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorisvanderbijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
