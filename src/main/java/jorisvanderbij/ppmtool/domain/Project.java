a2e781431eb64bff94a51aece975e42e4c1822fc
Jorisbij91 <joris_bij@hotmail.com>
2022-08-18 10:33:54 +0200

    added functionalities

--- src/main/java/jorisvanderbij/ppmtool/domain/Project.java
+++ src/main/java/jorisvanderbij/ppmtool/domain/Project.java
@@ -1,4 +1,38 @@
 package jorisvanderbij.ppmtool.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String projectDescription;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    public Project() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+
 }
